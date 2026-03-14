0397316ee593b9074a720555018aeca0f805c58f
ikngtty <ikngtty@gmail.com>
2026-03-15 01:58:39 +0900

    feat: プレイヤー検索

---
+++ docs/search-user/search_user.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import {
+  collection,
+  getDocsFromServer,
+  getFirestore,
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+
+import { CONFIG as FIREBASE_CONFIG } from "../_shared/firebase_util.js";
+
+const buttonSearch = document.getElementById("buttonSearch");
+const tableUsers = document.getElementById("tableUsers");
+
+const app = initializeApp(FIREBASE_CONFIG);
+const db = getFirestore(app);
+
+buttonSearch.addEventListener("click", async () => {
+  const tbody = tableUsers.tBodies[0];
+
+  // テーブルのリセット
+  tbody.replaceChildren();
+
+  const userProfilesSnapshot = await getDocsFromServer(
+    collection(db, "userProfiles"),
+  );
+
+  userProfilesSnapshot.forEach((doc) => {
+    const userProfile = doc.data();
+
+    const row = tbody.insertRow();
+    row.insertCell().textContent = userProfile.userName;
+    row.insertCell().textContent = userProfile.djName;
+    row.insertCell().textContent = userProfile.iidxId;
+    const buttonsToCompareCell = row.insertCell();
+    [
+      ["SP", userProfile.playdataSp],
+      ["DP", userProfile.playdataDp],
+    ].forEach(([playside, playdata]) => {
+      [1, 2].forEach((iPlayer) => {
+        const button = document.createElement("button");
+        button.textContent = `${playside}のデータをPlayer${iPlayer}にセット`;
+        button.addEventListener("click", () => {
+          localStorage.setItem(`iidxComparator.csv${iPlayer}`, playdata);
+          location.href = "../compare-playdata/";
+        });
+        buttonsToCompareCell.appendChild(button);
+      });
+      buttonsToCompareCell.appendChild(document.createElement("br"));
+    });
+  });
+});
