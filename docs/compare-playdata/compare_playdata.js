cf5018a37058cca9b966df74b2509c926a03ac03
ikngtty <ikngtty@gmail.com>
2026-03-30 00:33:43 +0900

    refactor: IIDX関係のロジックをsharedに括り出し

--- docs/compare-playdata/compare_playdata.js
+++ docs/compare-playdata/compare_playdata.js
@@ -1,60 +1,4 @@
-import { parseCsv } from "../shared/util.js";
-
-const VERSION_NAMES = [
-  "1st&substream",
-  "2nd style",
-  "3rd style",
-  "4th style",
-  "5th style",
-  "6th style",
-  "7th style",
-  "8th style",
-  "9th style",
-  "10th style",
-  "IIDX RED",
-  "HAPPY SKY",
-  "DistorteD",
-  "GOLD",
-  "DJ TROOPERS",
-  "EMPRESS",
-  "SIRIUS",
-  "Resort Anthem",
-  "Lincle",
-  "tricoro",
-  "SPADA",
-  "PENDUAL",
-  "copula",
-  "SINOBUZ",
-  "CANNON BALLERS",
-  "Rootage",
-  "HEROIC VERSE",
-  "BISTROVER",
-  "CastHour",
-  "RESIDENT",
-  "EPOLIS",
-  "Pinky Crush",
-  "Sparkle Shower",
-];
-
-const VERSION_NUMBER_FOR_NAME = (() => {
-  const m = new Map();
-  VERSION_NAMES.forEach((name, i) => {
-    m.set(name, i + 1);
-  });
-  return m;
-})();
-
-const SONG_TITLE_COLLATOR = new Intl.Collator("ja");
-
-const DIFFICULTIES = ["BEGINNER", "NORMAL", "HYPER", "ANOTHER", "LEGGENDARIA"];
-
-const DIFFICUTY_ORDER = (() => {
-  const m = new Map();
-  DIFFICULTIES.forEach((dif, i) => {
-    m.set(dif, i);
-  });
-  return m;
-})();
+import { compareChart, parseIidxCsv } from "../shared/iidx.js";
 
 const inputsCsv = [1, 2].map((i) => document.getElementById(`inputCsv${i}`));
 const buttonCompare = document.getElementById("buttonCompare");
@@ -105,42 +49,6 @@ function addComparisonRow(tbody, comparison) {
   row.insertCell().textContent = comparison.result2?.djLevel;
   row.insertCell().textContent = comparison.result1?.score;
   row.insertCell().textContent = comparison.result2?.score;
-}
-
-function compareChart(chart1, chart2) {
-  const songDelta = compareSong(chart1.song, chart2.song);
-  if (songDelta !== 0) {
-    return songDelta;
-  }
-
-  const [difOrder1, difOrder2] = [chart1, chart2].map((chart) => {
-    const dif = chart.difficulty;
-    const order = DIFFICUTY_ORDER.get(dif);
-    if (order == null) {
-      throw new Error(`unexpected difficulty: ${dif}`);
-    }
-    return order;
-  });
-
-  return difOrder1 - difOrder2;
-}
-
-function compareSong(song1, song2) {
-  const [ver1, ver2] = [song1, song2].map((song) => {
-    const name = song.version;
-    const num = VERSION_NUMBER_FOR_NAME.get(name);
-    if (num == null) {
-      throw new Error(`unexpected version name: ${name}`);
-    }
-    return num;
-  });
-
-  const verDelta = ver1 - ver2;
-  if (verDelta !== 0) {
-    return verDelta;
-  }
-
-  return SONG_TITLE_COLLATOR.compare(song1.title, song2.title);
 }
 
 function* makeRecordComparisons(compareChart, records1, records2) {
@@ -195,35 +103,4 @@ function makeRecordComparison(chart, result1, result2) {
     result1,
     result2,
   };
-}
-
-function* parseIidxCsv(text) {
-  for (const row of parseCsv(text)) {
-    const song = {
-      version: row["バージョン"],
-      title: row["タイトル"],
-    };
-
-    for (const difficulty of DIFFICULTIES) {
-      const level = row[`${difficulty} 難易度`];
-      if (level === "0") {
-        continue;
-      }
-
-      const chart = {
-        song,
-        difficulty,
-        level,
-      };
-
-      const result = {
-        clearType: row[`${difficulty} クリアタイプ`],
-        missCount: row[`${difficulty} ミスカウント`],
-        djLevel: row[`${difficulty} DJ LEVEL`],
-        score: row[`${difficulty} スコア`],
-      };
-
-      yield { chart, result };
-    }
-  }
 }
---
+++ docs/shared/iidx.js
@@ -0,0 +1,130 @@
+import { parseCsv } from "./util.js";
+
+export const VERSION_NAMES = [
+  "1st&substream",
+  "2nd style",
+  "3rd style",
+  "4th style",
+  "5th style",
+  "6th style",
+  "7th style",
+  "8th style",
+  "9th style",
+  "10th style",
+  "IIDX RED",
+  "HAPPY SKY",
+  "DistorteD",
+  "GOLD",
+  "DJ TROOPERS",
+  "EMPRESS",
+  "SIRIUS",
+  "Resort Anthem",
+  "Lincle",
+  "tricoro",
+  "SPADA",
+  "PENDUAL",
+  "copula",
+  "SINOBUZ",
+  "CANNON BALLERS",
+  "Rootage",
+  "HEROIC VERSE",
+  "BISTROVER",
+  "CastHour",
+  "RESIDENT",
+  "EPOLIS",
+  "Pinky Crush",
+  "Sparkle Shower",
+];
+
+export const DIFFICULTIES = [
+  "BEGINNER",
+  "NORMAL",
+  "HYPER",
+  "ANOTHER",
+  "LEGGENDARIA",
+];
+
+const VERSION_NUMBER_FOR_NAME = (() => {
+  const m = new Map();
+  VERSION_NAMES.forEach((name, i) => {
+    m.set(name, i + 1);
+  });
+  return m;
+})();
+
+const DIFFICUTY_ORDER = (() => {
+  const m = new Map();
+  DIFFICULTIES.forEach((dif, i) => {
+    m.set(dif, i);
+  });
+  return m;
+})();
+
+const SONG_TITLE_COLLATOR = new Intl.Collator("ja");
+
+export function* parseIidxCsv(text) {
+  for (const row of parseCsv(text)) {
+    const song = {
+      version: row["バージョン"],
+      title: row["タイトル"],
+    };
+
+    for (const difficulty of DIFFICULTIES) {
+      const level = row[`${difficulty} 難易度`];
+      if (level === "0") {
+        continue;
+      }
+
+      const chart = {
+        song,
+        difficulty,
+        level,
+      };
+
+      const result = {
+        clearType: row[`${difficulty} クリアタイプ`],
+        missCount: row[`${difficulty} ミスカウント`],
+        djLevel: row[`${difficulty} DJ LEVEL`],
+        score: row[`${difficulty} スコア`],
+      };
+
+      yield { chart, result };
+    }
+  }
+}
+
+export function compareChart(chart1, chart2) {
+  const songDelta = compareSong(chart1.song, chart2.song);
+  if (songDelta !== 0) {
+    return songDelta;
+  }
+
+  const [difOrder1, difOrder2] = [chart1, chart2].map((chart) => {
+    const dif = chart.difficulty;
+    const order = DIFFICUTY_ORDER.get(dif);
+    if (order == null) {
+      throw new Error(`unexpected difficulty: ${dif}`);
+    }
+    return order;
+  });
+
+  return difOrder1 - difOrder2;
+}
+
+export function compareSong(song1, song2) {
+  const [ver1, ver2] = [song1, song2].map((song) => {
+    const name = song.version;
+    const num = VERSION_NUMBER_FOR_NAME.get(name);
+    if (num == null) {
+      throw new Error(`unexpected version name: ${name}`);
+    }
+    return num;
+  });
+
+  const verDelta = ver1 - ver2;
+  if (verDelta !== 0) {
+    return verDelta;
+  }
+
+  return SONG_TITLE_COLLATOR.compare(song1.title, song2.title);
+}
