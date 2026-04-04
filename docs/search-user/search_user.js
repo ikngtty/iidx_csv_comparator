04f889695b1a38c34c4e22c89ae3760e1e47a1fa
ikngtty <ikngtty@gmail.com>
2026-04-04 18:47:04 +0900

    feat: ユーザー検索のページネーション

--- docs/search-user/search_user.js
+++ docs/search-user/search_user.js
@@ -4,30 +4,75 @@ import {
   getDocFromServer,
   getDocsFromServer,
   getFirestore,
+  limit,
+  orderBy,
+  startAt,
+  query,
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 import { CONFIG as FIREBASE_CONFIG } from "../shared/firebase_util.js";
 import { getPlaydataDocRef } from "../shared/repository.js";
 import { firestoreTimestampToString } from "../shared/util.js";
 
+const USER_PROFILE_COUNT_PER_PAGE = 10;
+
 const buttonSearch = document.getElementById("buttonSearch");
 const tableUsers = document.getElementById("tableUsers");
+const buttonSearchNext = document.getElementById("buttonSearchNext");
 
 const app = initializeApp(FIREBASE_CONFIG);
 const db = getFirestore(app);
+
+let lastSearch = {
+  query: null,
+  nextDoc: null,
+};
 
 buttonSearch.addEventListener("click", async () => {
   // テーブルのリセット
   const tbody = tableUsers.tBodies[0];
   tbody.replaceChildren();
 
-  const userProfilesSnapshot = await getDocsFromServer(
+  const searchQuery = query(
     collection(db, "userProfiles"),
+    orderBy("createdAt", "desc"),
+    limit(USER_PROFILE_COUNT_PER_PAGE + 1),
   );
 
-  userProfilesSnapshot.forEach((userProfileDoc) => {
-    addUserRow(tbody, userProfileDoc.id, userProfileDoc.data());
-  });
+  const userProfilesSnapshot = await getDocsFromServer(searchQuery);
+
+  lastSearch.query = searchQuery;
+  if (userProfilesSnapshot.size > USER_PROFILE_COUNT_PER_PAGE) {
+    lastSearch.nextDoc = userProfilesSnapshot.docs[USER_PROFILE_COUNT_PER_PAGE];
+  } else {
+    lastSearch.nextDoc = null;
+  }
+  renderForLastSearch(lastSearch);
+
+  userProfilesSnapshot.docs
+    .slice(0, USER_PROFILE_COUNT_PER_PAGE)
+    .forEach((userProfileDoc) => {
+      addUserRow(tbody, userProfileDoc.id, userProfileDoc.data());
+    });
+});
+
+buttonSearchNext.addEventListener("click", async () => {
+  const searchQuery = query(lastSearch.query, startAt(lastSearch.nextDoc));
+  const userProfilesSnapshot = await getDocsFromServer(searchQuery);
+
+  if (userProfilesSnapshot.size > USER_PROFILE_COUNT_PER_PAGE) {
+    lastSearch.nextDoc = userProfilesSnapshot.docs[USER_PROFILE_COUNT_PER_PAGE];
+  } else {
+    lastSearch.nextDoc = null;
+  }
+  renderForLastSearch(lastSearch);
+
+  const tbody = tableUsers.tBodies[0];
+  userProfilesSnapshot.docs
+    .slice(0, USER_PROFILE_COUNT_PER_PAGE)
+    .forEach((userProfileDoc) => {
+      addUserRow(tbody, userProfileDoc.id, userProfileDoc.data());
+    });
 });
 
 async function handleButtonCompareClick(event) {
@@ -39,6 +84,10 @@ async function handleButtonCompareClick(event) {
 
   localStorage.setItem(`iidxComparator.csv${playerIndex}`, playdata);
   location.href = "../compare-playdata/";
+}
+
+function renderForLastSearch(lastSearch) {
+  buttonSearchNext.disabled = lastSearch.nextDoc == null;
 }
 
 function addUserRow(tbody, userId, userProfile) {
